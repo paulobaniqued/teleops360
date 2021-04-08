81767e5695e846454c69db4aa591ac7376882def
Paul Dominick Baniqued <47854500+paulbaniqued@users.noreply.github.com>
2021-04-08 10:18:09 +0100

    Working webcam-Unity stream

---
+++ teleops360/Assets/Scripts/testScriptBasic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScriptBasic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        Debug.Log("Number of web cams connected: " + devices.Length);
+        Renderer rend = this.GetComponentInChildren<Renderer>();
+
+        WebCamTexture mycam = new WebCamTexture();
+        string camName = devices[0].name; // 0:laptop webcam, 1:Theta V/Z1 cam
+        Debug.Log("The webcam name is " + camName);
+        mycam.deviceName = camName;
+        rend.material.mainTexture = mycam;
+
+        mycam.Play();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
